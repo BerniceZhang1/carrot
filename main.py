b2be511fed93347f65b6118f8eb7ce99aafcefdd
BerniceZhang1 <145476013+BerniceZhang1@users.noreply.github.com>
2024-06-03 10:59:53 -0400

    6/3

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
 import random
 import time
+from random import randint
 from button import Button
 from water import Water
-# from bath_tub import Bathtub
-from bucket import Bucket
-# from duck import Duck
+from fish import Fish
 
 
 # set up pygame modules
@@ -20,30 +19,27 @@ SCREEN_HEIGHT = 649
 SCREEN_WIDTH = 1152
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
-# background = pygame.image.load("farm.png")
 start_button = pygame.image.load("play_button.png")
 water = pygame.image.load("water.png")
+# makes image transparent
 water.set_alpha(160)
-bucket = pygame.image.load("bucket.png")
-# farm = pygame.image.load()
 title = "Carrot"
 start = "Play"
 click = False
-start_time = time.time()
 
-#render
+# render
 display_title = my_font.render(title, True, (0, 0, 0))
 display_start = my_font_two.render(start, True, (0, 0, 0))
 
 s = Button(645, 450)
-b = Bucket(560, 640)
+# b = Bucket(560, 640)
 
 # variable for direction
 direction = 1
 
-# object = Duck(340, 580)
+block = Fish(50, 50)
 
-#Starting speed
+# starting speed
 speed_x = 3
 speed_y = 4
 
@@ -58,8 +54,20 @@ while run:
     if keys[pygame.K_a]:
         b.move_direction("left")
 
-    # if object.left <= 20:
+    #side
+    if block.left <= 20 or block.right >= 1130:
+        direction = direction * -1
+        speed_x = randint(1, 8) * direction
+        speed_y = randint(1, 8) * direction
 
+    #bottom and top
+    if block.top <= 20 or block.bottom >= 1130:
+        direction = direction * -1
+        speed_x = randint(1, 8) * direction
+        speed_y = randint(1, 8) * direction
+
+    block.left += speed_x
+    block.top += speed_y
 
     # --- Main event loop
     current_time = time.time()
@@ -76,6 +84,7 @@ while run:
 
     ## FILL SCREEN, and BLIT here ##
 
+
     if click == False:
         # screen.blit(background, (0, 0))
         screen.fill((255, 255, 255))
@@ -84,7 +93,8 @@ while run:
     if click == True:
         screen.fill((0, 0, 0))
         screen.blit(water, (0, 0))
-        screen.blit(b.image, b.rect)
+        pygame.draw.rect(screen, (0, 255, 0), rectangle)
+        # screen.blit(b.image, b.rect)
         # current_time = time.time()
         # time_elapsed = round(10 - (current_time - start_time), 2)
         # display_time = my_font_two.render("Time Elapsed: " + str(time_elapsed) + "s", True, (255, 255, 255))
