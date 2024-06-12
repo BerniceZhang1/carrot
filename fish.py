58ddce441ce425a978eab58b14b50b95684a0acd
BerniceZhang1 <145476013+BerniceZhang1@users.noreply.github.com>
2024-06-11 23:40:01 -0400

    6/11

--- fish.py
+++ fish.py
@@ -17,8 +17,8 @@ class Fish:
         (random.randint(0, self.screen_dimensions[0] - self.image_size[0]), -self.image_size[1]),
         (random.randint(0, self.screen_dimensions[0] - self.image_size[0]), self.screen_dimensions[1])
         ]
-        self.x = starting_pos[self.dir_going][0]
-        self.y = starting_pos[self.dir_going][1]
+        self.x = self.starting_pos[self.dir_going][0]
+        self.y = self.starting_pos[self.dir_going][1]
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
         self.delta = 2
         self.left = True
@@ -47,42 +47,21 @@ class Fish:
             self.x -= self.delta
         elif self.direction == "right":
             self.x += self.delta
-        elif self.direction == "up":
+        elif self.direction ==  "up":
             self.y -= self.delta
         elif self.direction == "down":
             self.y += self.delta
 
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
 
-    def fish_position(self):
-        self.x = random.randint(-100, 1300)
-        self.y = random.randint(-100, 750)
-        if self.x <= 1300 and self.y == -100:
-            side = top
-        if self.x == -100 and self.y <= 750:
-            side = left
-        if self.x == 1300 and self.y <= 750:
-            side = right
-        if self.x <= 1300 and self.y == 750:
-            side = bottom
-
-    def switch_image(self, side):
-        if side == top:
-            if not self.left:
-                image_number = 1
-            self.image = pygame.image.load(self.image_list[image_number])
-            # self.rescale_image(self.image)
-            self.image_size = self.image.get_size()
-            self.left = not self.left
-
-        if side == bottom:
-            image_number = 0
-            if not self.left:
-                image_number = 1
-            self.image = pygame.image.load(self.image_list[image_number])
-            # self.rescale_image(self.image)
-            self.image_size = self.image.get_size()
-            self.left = not self.left
+    def switch_image(self):
+        image_number = 0
+        if not self.left:
+            image_number = 1
+        self.image = pygame.image.load(self.image_list[image_number])
+        # self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.left = not self.left
 
 
 
--- main.py
+++ main.py
@@ -9,34 +9,33 @@ import random
 # set up pygame modules
 pygame.init()
 pygame.font.init()
-my_font = pygame.font.SysFont("Comic Sans", 200)
-my_font_two = pygame.font.SysFont("Comic Sans", 30)
+my_font = pygame.font.SysFont("inkfree", 220)
+
+print(pygame.font.get_fonts())
 pygame.display.set_caption("Farm")
 
 # set up variables for the display
-# SCREEN_HEIGHT = 649
-# SCREEN_WIDTH = 1152
-SCREEN_HEIGHT = 800
-SCREEN_WIDTH = 1300
+SCREEN_HEIGHT = 640
+SCREEN_WIDTH = 1200
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
+background = pygame.image.load("background.png")
 start_button = pygame.image.load("play_button.png")
 water = pygame.image.load("water.png")
 # makes image transparent
-water.set_alpha(200)
-title = "Carrot"
-start = "Play"
+water.set_alpha(250)
+# title = "𝐿𝑒 𝒫𝑜𝒾𝓈𝓈𝑜𝓃"
+title = "Le Poisson"
 click = False
 
-# rectangle = pygame.rect((0, 0, 1250, 750)
-# rectangle.center = (576, 324.5)
 
 # render
-display_title = my_font.render(title, True, (0, 0, 0))
-display_start = my_font_two.render(start, True, (0, 0, 0))
+display_title = my_font.render(title, True, (255, 255, 255))
 
-s = Button(645, 450)
-fish = Fish(780, 450, size)
+
+s = Button(475, 400)
+fish = Fish(size)
+# fish = Fish(780, 450, size)
 
 
 fishes = []
@@ -64,7 +63,8 @@ while run:
 
     for fish in fishes:
         Fish.check_reached_coord(fish)
-        if not(fish.reached_coord):
+        # if not (fish.reached_coord):
+        if not(Fish.check_reached_coord):
             Fish.move_fish(fish)
         else:
             fishes.pop(fishes.index(fish))
@@ -77,8 +77,8 @@ while run:
 
     ## FILL SCREEN, and BLIT here ##
     if click == False:
-        screen.fill((255, 255, 255))
-        screen.blit(display_title, (500, 30))
+        screen.blit(background, (0, 0))
+        screen.blit(display_title, (120, 130))
         screen.blit(s.image, s.rect)
     if click == True:
         screen.fill((0, 0, 0))
